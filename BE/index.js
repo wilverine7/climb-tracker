474932a4764c312156273a8cfcac19f2aaac368f
wilverine7 <95718700+wilverine7@users.noreply.github.com>
2024-11-03 14:42:14 -0700

    comment

--- BE/index.js
+++ BE/index.js
@@ -25,6 +25,9 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
+
+
+//examples
 // await createUser({
 //             username: 'johndoe',
 //             email: 'john@example.com',
